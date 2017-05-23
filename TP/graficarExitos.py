7859ce6381a9ffc5531ee91c46bbe12039be76e3
ignacio roberto rodriguez <ignacio.igrodriguez@mercadolibre.com>
2017-05-23 09:43:37 -0300

    Agrego funcion para graficar los exitos

---
+++ TP/graficarExitos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+import sys
+import matplotlib.pyplot as plt
+
+os.system("python kfold.py 0 41 15")
+archivoR = open("cantExitos.out","r")
+exitosNorma = archivoR.readline()
+exitosNorma = [float(i) for i in exitosNorma.split(" ")]
+exitosCota = archivoR.readline()
+exitosCota = [float(i) for i in exitosCota.split(" ")]
+keys=range(1,1501)
+
+plt.xlabel("norma o cota")
+plt.ylabel("hitrate") 
+plt.title("hitrate segun el numero que se usa como cota en Hamming o la norma con la que se calcula la distancia");
+plt.plot(keys,exitosCota,label='Funcion de Hamming');
+plt.plot(keys,exitosNorma,label='Distancia Norma');
+plt.show();
