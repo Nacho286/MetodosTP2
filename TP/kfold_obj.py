140eef82bec93b49e1b1af80aebeefebbc0f75bc
FerminT <FerminT@users.noreply.github.com>
2017-06-15 17:17:25 -0300

    kfold modificado para objetos
    Todas las personas que no hayan sido incluidas en el entrenamiento, se incluyen como prueba. Además, se agregan 10 imágenes de objetos.

---
+++ TP/kfold_obj.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import os
+import sys
+import random as ran
+
+size = int(sys.argv[1])
+cantPersonas = int(sys.argv[2])
+cantAutovalores = int(sys.argv[3])
+
+personas=ran.sample(range(1,42),cantPersonas)
+kfold1={}
+kfold2={}
+kfold3={}	
+kfold4={}
+kfold5={}
+kfolds=[kfold1,kfold2,kfold3,kfold4,kfold5]
+total="resultadosGenerales.out"
+archivoA=open(total,"w")
+listaResultados=[[0.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0]]
+for j in personas:
+	fotos=range(1,11)
+	for k in range(0,5):
+		fotosSeleccionadas=ran.sample(fotos,2)
+		fotos=[x for x in fotos if x not in fotosSeleccionadas]
+		kfolds[k][j]=fotosSeleccionadas
+for j in range(0,5):
+	archivo= "pruebaKFold"+str(j+1)+"_"+ ("Big_" if size==0 else "Red_") + str(cantPersonas)+"_"+str(cantAutovalores)+".in"
+	archivoW=open(archivo,"w")
+	archivoW.write("../data/ImagenesCaras"+(""if size==0 else"Red")+"/ "+ ("112 92 " if size==0 else "28 23 ") + str(cantPersonas)+" 8 "+str(cantAutovalores)+"\n")
+	for persona in personas:
+		aux="s"+str(persona)+"/ "
+		for i in range(0,5):
+			if (i!=j):
+				kfold = kfolds[i]
+				for foto in kfold[persona]:
+					aux+=str(foto)+" "
+		aux+="\n"
+		archivoW.write(aux)
+	cantObjetos = 10
+	ntest = cantPersonas * 2 + (41 - cantPersonas) * 10 + cantObjetos
+	archivoW.write(str(ntest)+"\n")
+	p=0
+	for persona in personas:
+		p+=1
+		for i in range(0,5):
+			if (i==j):
+				kfold = kfolds[i]
+				for foto in kfold[persona]: 
+					aux="../data/ImagenesCaras"+(""if size==0 else"Red")+"/s"+str(persona)+"/"+str(foto)+".pgm 1 \n"
+					archivoW.write(aux)
+	for i in range(1, 42):
+		if i not in personas:
+			for foto in range(1, 11):
+				aux="../data/ImagenesCaras" + ("" if size == 0 else "Red") + "/s" + str(i) + "/" + str(foto) + ".pgm 1 \n"
+				archivoW.write(aux)
+	for i in range(1, 11):
+		aux="../data/ImagenesCaras" + ("" if size == 0 else "Red") + "/obj/" + str(i) + ".pgm 0 \n"
+		archivoW.write(aux)
+	archivoW.close()
+	#os.system("./main "+archivo+" test"+str(j+1)+".out")
+	#resultado="resultados.out"
+	#archivoR=open(resultado,"r")
+	#for i in range(0,3):
+	#	linea=archivoR.readline()
+	#	linea=linea.split(" ")
+	#	for k in range(0,5):
+	#		listaResultados[i][k]+=float(linea[k])
+#for i in range(0,3):
+	#for k in range(0,5):
+	#	archivoA.write(str(listaResultados[i][k]/float(5))+" ")
+	#archivoA.write("\n")
